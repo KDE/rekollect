40f033bfe6e0d08017bc2d0b5073b7b4232aeae2
Jason Jackson <jacksonje@gmail.com>
2011-03-07 13:20:40 -0600

    Update copyright dates.

--- src/notebrowserwindow.h
+++ src/notebrowserwindow.h
@@ -1,6 +1,6 @@
 /*
     Rekollect: A note taking application
-    Copyright (C) 2009, 2010  Jason Jackson <jacksonje@gmail.com>
+    Copyright (C) 2009, 2010, 2011  Jason Jackson <jacksonje@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
