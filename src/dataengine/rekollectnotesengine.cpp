fcef5558268fcd56fb70d51fabd1ad197de3c8a7
Jason Jackson <jacksonje@gmail.com>
2011-03-09 08:10:27 -0600

    Create a plasma dataengine to retrieve metadata about rekollect's notes.

---
+++ src/dataengine/rekollectnotesengine.cpp
@@ -0,0 +1,130 @@
+/*
+    Rekollect: A note taking application
+    Copyright (C) 2011  Jason Jackson <jacksonje@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "rekollectnotesengine.h"
+
+#include <QtCore/QFile>
+
+#include <KDirWatch>
+#include <KStandardDirs>
+#include <KFileItem>
+
+RekollectNotesEngine::RekollectNotesEngine(QObject *parent, const QVariantList &args)
+: Plasma::DataEngine(parent, args)
+{
+    Q_UNUSED(args)
+    setMinimumPollingInterval(333);
+    m_dirWatch = new KDirWatch(this);
+}
+
+QStringList RekollectNotesEngine::sources() const
+{
+    QStringList sourcesList;
+    foreach (const MetaData metaData, m_noteMetaData) {
+        sourcesList << metaData.fileName;
+    }
+    sourcesList << "DEBUG";
+    return sourcesList;
+}
+
+void RekollectNotesEngine::init()
+{
+    #ifndef DEBUG
+    KGlobal::dirs()->addResourceType("app_notes", "data", "rekollect/notes");
+    #else
+    KGlobal::dirs()->addResourceType("app_notes", "data", "rekollect/notesdev");
+    #endif
+    loadNoteMetaData();
+    startDirWatch();
+}
+
+bool RekollectNotesEngine::sourceRequestEvent(const QString &name)
+{
+    return updateSourceEvent(name);
+}
+
+bool RekollectNotesEngine::updateSourceEvent(const QString &name)
+{
+    if (m_noteMetaData.contains(name)) {
+        setData(name, I18N_NOOP("File Name"), m_noteMetaData.value(name).fileName);
+        setData(name, I18N_NOOP("Document Name"), m_noteMetaData.value(name).documentName);
+        setData(name, I18N_NOOP("Modification Time"), m_noteMetaData.value(name).modificationTime.dateTime());
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void RekollectNotesEngine::loadNoteMetaData()
+{
+    QStringList noteList = KGlobal::dirs()->findAllResources("app_notes", QString());
+    foreach (const QString &notePath, noteList) {
+        MetaData metaData = noteMetaData(notePath);
+        if (!metaData.documentName.isEmpty()) {
+            m_noteMetaData.insert(metaData.fileName, metaData);
+        }
+    }
+}
+
+void RekollectNotesEngine::startDirWatch()
+{
+    QStringList noteDirs = KGlobal::dirs()->findDirs("app_notes", QString());
+    foreach (const QString &noteDir, noteDirs) {
+        m_dirWatch->addDir(noteDir, KDirWatch::WatchDirOnly);
+    }
+    connect(m_dirWatch, SIGNAL(dirty(QString)), this, SLOT(reloadNoteMetaData()));
+}
+
+MetaData RekollectNotesEngine::noteMetaData(const QString& fileName)
+{
+    QFile file(fileName);
+    file.open(QIODevice::ReadOnly);
+
+    MetaData metaData;
+
+    m_xml.setDevice(&file);
+    if (m_xml.readNextStartElement()) {
+        if (m_xml.name() == "note" && m_xml.attributes().value("version") == "1") {
+            do {
+                m_xml.readNextStartElement();
+            } while (m_xml.name() != "text");
+            metaData.documentName = m_xml.readElementText();
+            KFileItem item(KFileItem::Unknown, KFileItem::Unknown, KUrl(file.fileName()));
+            metaData.fileName = file.fileName();
+            metaData.modificationTime = item.time(KFileItem::ModificationTime);
+        } else {
+            m_xml.raiseError(i18nc("@info Incorrect file type error", "The file is not a Rekollect Note version 1 file"));
+        }
+    }
+
+    return metaData;
+}
+
+void RekollectNotesEngine::reloadNoteMetaData()
+{
+    m_noteMetaData.clear();
+    loadNoteMetaData();
+    updateSourceEvent("notes");
+    emit scheduleSourcesUpdated();
+}
+
+K_EXPORT_PLASMA_DATAENGINE(rekollectnotes, RekollectNotesEngine)
+
+#include "rekollectnotesengine.moc"
---
+++ src/dataengine/rekollectnotesengine.h
@@ -0,0 +1,58 @@
+/*
+    Rekollect: A note taking application
+    Copyright (C) 2011  Jason Jackson <jacksonje@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#ifndef REKOLLECTNOTESENGINE_H
+#define REKOLLECTNOTESENGINE_H
+
+#include "metadata.h"
+
+#include <QtCore/QXmlStreamReader>
+#include <Plasma/DataEngine>
+
+class KDirWatch;
+
+class RekollectNotesEngine : public Plasma::DataEngine
+{
+    Q_OBJECT
+public:
+    RekollectNotesEngine(QObject *parent, const QVariantList &args);
+
+    QStringList sources() const;
+    void init();
+
+protected:
+    bool sourceRequestEvent(const QString &name);
+    bool updateSourceEvent(const QString &name);
+
+private:
+    void loadNoteMetaData();
+    void startDirWatch();
+    MetaData noteMetaData(const QString &fileName);
+
+private slots:
+    void reloadNoteMetaData();
+
+private:
+    QHash<QString, MetaData> m_noteMetaData;
+    KDirWatch *m_dirWatch;
+    QXmlStreamReader m_xml;
+};
+
+#endif // REKOLLECTNOTESENGINE_H
